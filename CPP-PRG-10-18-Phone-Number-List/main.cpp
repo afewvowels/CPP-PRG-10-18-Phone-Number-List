85b08a0291ce203a63d4690a6a0fe028399299c3
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-05 13:23:53 -0500

    Prompt written.

--- CPP-PRG-10-18-Phone-Number-List/main.cpp
+++ CPP-PRG-10-18-Phone-Number-List/main.cpp
@@ -5,11 +5,37 @@
 //  Created by Keith Smith on 11/5/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that has an array of at least 10 string objects that hold people's
+//  names and phone numbers. You may make up your own strings, or use the following:
+//
+//      "Alejandra Cruz, 555-1223"
+//      "Joe Looney, 555-0097"
+//      "Geri Palmer, 555-8787"
+//      "Li Chen, 555-1212"
+//      "Holly Gaddis, 555-8878"
+//      "Sam Wiggins, 555-0998"
+//      "Bob Kain, 555-8712"
+//      "Tim Haynes, 555-7676"
+//      "Warren Gaddis, 555-9037"
+//      "Jean James, 555-4939"
+//      "Ron Palmer, 555-2783"
+//
+//  The program should ask the user to enter a name or partial name to search for in the
+//  array. Any entries in the array that match the string entered should be displayed. For
+//  example, if the use enters "Palmer" the program should display the following names
+//  from the list:
+//
+//      Geri Palmer, 555-8787
+//      Ron Palmer, 555-2783
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
+    
     return 0;
 }
